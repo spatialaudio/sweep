d5b5896d0b1aaf4b979c992b67854e2b97942610
franzpl <franz.plocksties@uni-rostock.de>
2015-03-24 01:25:12 +0100

    Change code-style and logic

--- generation.py
+++ generation.py
@@ -1,25 +1,20 @@
 import numpy as np
-import show
 
 
 def lin_sweep(fstart, fstop, sweep_time, fs):
-    if fs / 2 < fstop:
-        print("fstop must be less than fs/2")
+    if fstop > fs / 2:
+        raise ValueError("fstop must not be greater than fs/2")
     t = np.arange(0, sweep_time, 1 / fs)
     x = np.sin(
         2 * np.pi * ((fstop - fstart) /
                      (2 * sweep_time) * t ** 2 + fstart * t))
-    return (x, sweep_time, fs)
+    return x
 
 
 def log_sweep(fstart, fstop, sweep_time, fs):
-    if fs / 2 < fstop:
-        print("fstop must be less than fs/2")
+    if fstop > fs / 2:
+        raise ValueError("fstop must not be greater than fs/2")
     t = np.arange(0, sweep_time, 1 / fs)
-    x = np.sin(2 * np.pi * sweep_time * fstart / np.log(fstop / fstart)
-               * np.exp(t / (sweep_time) * np.log(fstop / fstart) - 1))
-    return (x, sweep_time, fs)
-
-# for example
-# x, sweeptime, fs = lin_sweep(10, 10000, 1, 20000)
-# show.sweep(x, y, z)
+    x = np.sin(2 * np.pi * sweep_time * fstart / np.log(fstop / fstart) *
+               np.exp(t / (sweep_time) * np.log(fstop / fstart) - 1))
+    return x
