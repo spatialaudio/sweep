75c83d82e71d9075caaa8e8817017129d3dbb0e1
franzpl <franz.plocksties@uni-rostock.de>
2015-03-31 01:02:20 +0200

    Add new test function

--- example.py
+++ example.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 import generation
 import show
+import deconvolution
+import numpy as np
+
 
 # For example
 
 """Save the return value in a new variable which is the sweep
  vector. """
 
-x = generation.log_sweep(1, 1000, 2, 44100)
+x = generation.log_sweep(1, 1000, 1, 44100)
 
-"""We created a vector which sweeps from 1 Hz to 1000 Hz in 2
-seconds at a sampling frequency of 44.1 kHz.
+"""We created a vector which sweeps from 1 Hz to 1000 Hz in 1
+second at a sampling frequency of 44.1 kHz.
 """
-show.sweep(x, 2, 44100, "tfdomain")
+# show.plot_tf(x, 44100)
+
+"""Test functions for deconvolution"""
+
+y = generation.log_sweep(1, 1000, 1, 44100)
+noise = np.random.normal(0, 0.07, len(y))
+noisy_signal = y + noise
+deconvolution.deconv(x, noisy_signal, 44100)
