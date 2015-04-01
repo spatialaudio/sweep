73827252f41ff2bf93b04a02f2fa6bad5491383b
franzpl <franz.plocksties@uni-rostock.de>
2015-04-01 17:17:18 +0200

    Change examples

--- example.py
+++ example.py
@@ -1,25 +1,23 @@
+"""A list of examples."""
+
 #!/usr/bin/env python3
 import generation
-import show
-import deconvolution
+import plotting
 import numpy as np
 
 
-# For example
-
-"""Save the return value in a new variable which is the sweep
- vector. """
-
 x = generation.log_sweep(1, 1000, 1, 44100)
-
 """We created a vector which sweeps from 1 Hz to 1000 Hz in 1
-second at a sampling frequency of 44.1 kHz.
-"""
-# show.plot_tf(x, 44100)
+second at a sampling frequency of 44.1 kHz. Now you can plot it
+in several domains."""
 
-"""Test functions for deconvolution"""
+# plotting.plot_freq(x,44100)
+# plotting._show_it()
+
+"""Test functions for response calculation."""
 
 y = generation.log_sweep(1, 1000, 1, 44100)
 noise = np.random.normal(0, 0.07, len(y))
 noisy_signal = y + noise
-deconvolution.deconv(x, noisy_signal, 44100)
+plotting.plot_ifresponse(x, noisy_signal, 44100)
+plotting._show_it()
