bdb48a9f8cf10bcf922182898f87439430ab7e45
franzpl <franz.plocksties@uni-rostock.de>
2015-04-09 18:59:54 +0200

    Adding new examples

--- example.py
+++ example.py
@@ -1,23 +1,28 @@
+#!/usr/bin/env python3
 """A list of examples."""
 
-#!/usr/bin/env python3
 import generation
 import plotting
 import numpy as np
+import matplotlib.pyplot as plt
+import calculation
 
 
 x = generation.log_sweep(1, 1000, 1, 44100)
-"""We created a vector which sweeps from 1 Hz to 1000 Hz in 1
-second at a sampling frequency of 44.1 kHz. Now you can plot it
-in several domains."""
 
-# plotting.plot_freq(x,44100)
-# plotting._show_it()
+# We created a vector which sweeps from 1 Hz to 1000 Hz in 1
+# second at a sampling frequency of 44.1 kHz. Now you can plot it
+# in several domains.
 
-"""Test functions for response calculation."""
+# plotting.plot_time(x,44100)
+
+
+# Test functions for response and snr calculations.
 
 y = generation.log_sweep(1, 1000, 1, 44100)
 noise = np.random.normal(0, 0.07, len(y))
 noisy_signal = y + noise
-plotting.plot_ifresponse(x, noisy_signal, 44100)
-plotting._show_it()
+z = calculation.spectral_division(x, noisy_signal)
+# print(calculation.snr_db(x,noise))
+plotting.plot_tf(z, 44100)
+plt.show()
