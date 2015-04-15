0b425ccac392e0618615b0b7501524d9a1c55d2d
franzpl <franz.plocksties@uni-rostock.de>
2015-04-16 01:31:47 +0200

    Change plotting examples

--- example.py
+++ example.py
@@ -5,6 +5,7 @@ import generation
 import plotting
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.mlab as ml
 import calculation
 
 
@@ -14,15 +15,25 @@ x = generation.log_sweep(1, 1000, 1, 44100)
 # second at a sampling frequency of 44.1 kHz. Now you can plot it
 # in several domains.
 
-# plotting.plot_time(x,44100)
+
+ax = plotting.plot_freq(
+    x,
+     fs=44100,
+     scale='dB',
+     side='onesided',
+     bode='mag')
+ax.set_xscale('log')  # if side = 'twosided' you have to disable this line
+plt.show()
 
 
 # Test functions for response and snr calculations.
 
 y = generation.log_sweep(1, 1000, 1, 44100)
-noise = np.random.normal(0, 0.07, len(y))
+noise = np.random.normal(0, 0.3, len(y))
 noisy_signal = y + noise
 z = calculation.spectral_division(x, noisy_signal)
+
 # print(calculation.snr_db(x,noise))
-plotting.plot_tf(z, 44100)
-plt.show()
+# ax = plotting.plot_freq(z, fs =44100, scale = 'dB')
+# ax.set_xscale('log')
+# plt.show()
