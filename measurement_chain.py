44fa26aedd688157988877c17c1d0c18a04f5ed0
franzpl <franz.plocksties@uni-rostock.de>
2015-05-25 18:09:33 +0200

    Modularize ir-functions

--- measurement_chain.py
+++ measurement_chain.py
@@ -1,85 +1,83 @@
 from scipy.signal import butter, lfilter, fftconvolve
 import numpy as np
+import generation
 
 
 # superior DUT begin
 
 
 def bandpass(lower_bound, higher_bound, fs, order):
-    def dummy(data):
+    def inner(data):
         b, a = _butter_bandpass(lower_bound, higher_bound, fs, order)
         return lfilter(b, a, data)
-    dummy.name = "{}.-order bandpass with lower_bound = {} Hz and higher_bound = {} Hz".format(
+    inner.name = "{}.-order bandpass with lower_bound = {} Hz and higher_bound = {} Hz".format(
         order, lower_bound, higher_bound)
-    dummy.filename = "bp{}_{}_{}".format(order, lower_bound, higher_bound)
-    return dummy
+    inner.filename = "bp{}_{}_{}".format(order, lower_bound, higher_bound)
+    return inner
 
 
-def additive_noise(standard_deviation, seed):
-    def dummy(data):
+def additive_noise(noise_level_dB, seed=None):
+    if seed is None:
+        seed = 1
         np.random.seed(seed)
+
+    def inner(data):
+        standard_deviation = 10 ** (noise_level_dB / 20)
         return data + np.random.normal(0, standard_deviation, len(data))
-    dummy.name = "additive noise with {} standard deviation".format(
-        standard_deviation)
-    dummy.filename = "{}noise".format(standard_deviation)
-    return dummy
+    inner.name = "additive noise with {} dB noise level".format(
+        noise_level_dB)
+    inner.filename = "{}noise".format(noise_level_dB)
+    return inner
 
 
 def anti_aliasing_filter(cutoff, fs, order):
-    def dummy(data):
+    def inner(data):
         b, a = _butter_lowpass(cutoff, fs, order=order)
         return lfilter(b, a, data)
-    dummy.name = "anti-aliasing filter with fc = {} and {}.order".format(
+    inner.name = "anti-aliasing filter with fc = {} and {}.order".format(
         cutoff, order)
-    dummy.filename = "aaf_fc{}_order{} ".format(cutoff, order)
-    return dummy
+    inner.filename = "aaf_fc{}_order{} ".format(cutoff, order)
+    return inner
 
 
 def add_gain(gain_level_dB):
-    def dummy(data):
+    def inner(data):
         return 10 ** (gain_level_in_dB / 20) * data
-    dummy.name = "add {} dB to the signal".format(gain_level_dB)
-    dummy.filename = "addgain{}dB".format(gain_level_dB)
-    return dummy
+    inner.name = "add {} dB to the signal".format(gain_level_dB)
+    inner.filename = "addgain{}dB".format(gain_level_dB)
+    return inner
 
 
 # subordinate DUT begin
 
-def diracs(dirac_positions_array):
-    def dummy(data):
-        h = np.zeros(len(data))
-        for i in dirac_positions_array:
-            h[i] = 1
-        return fftconvolve(data, h)[:len(data)]
-    dummy.name = "dirac-filter positions:{}".format(dirac_positions_array)
-    dummy.filename = "dirac{}".format(dirac_positions_array)
-    return dummy
+#~ def diracs(dirac_positions_array):
+    #~ def inner(data):
+        #~ h = np.zeros(len(data))
+        #~ for i in dirac_positions_array:
+            #~ h[i] = 1
+        #~ return fftconvolve(data, h)[:len(data)]
+    #~ inner.name = "dirac-filter positions:{}".format(dirac_positions_array)
+    #~ inner.filename = "dirac{}".format(dirac_positions_array)
+    #~ return inner
 
 
 def moving_average(N):
     # for more information:
     # http://stackoverflow.com/questions/13728392/moving-average-or-running-mean
-    def dummy(data):
+    def inner(data):
         cumsum = np.cumsum(np.insert(data, 0, 0))
         return (cumsum[N:] - cumsum[:-N]) / N
-    dummy.name = "moving average filter with {} samples".format(N)
-    dummy.filename = "maf{}".format(N)
-    return dummy
+    inner.name = "moving average filter with {} samples".format(N)
+    inner.filename = "maf{}".format(N)
+    return inner
 
 
-def exponential_decay(standard_deviation, lifetime, seed):
-    def dummy(data):
-        t = np.arange(len(data)) / len(data)
-        np.random.seed(seed)
-        noise = np.random.normal(0, standard_deviation, len(data))
-        exponential_fading_noise = noise * np.exp(-t / lifetime)
-        exponential_fading_noise[0] = 1
-        return fftconvolve(data, exponential_fading_noise)[:len(data)]
-    dummy.name = "exponential decay with {} standard deviation and {} lifetime".format(
-        standard_deviation, lifetime)
-    dummy.filename = "exponential_decay_{}sd_{}lt".format(
-        standard_deviation, lifetime)
-    return dummy
+def take_ir(ir):
+    def inner(data):
+        return fftconvolve(data, ir)[:len(data)]
+    inner.name = "{}".format("IR")
+    inner.filename = "{}".format("IR")
+    return inner
 
 # subordinate DUT end
 
@@ -87,7 +85,30 @@ def exponential_decay(standard_deviation, lifetime, seed):
 # superior DUT end
 
 
+# generate some impulse responses begin
+
+def exponential_decay(excitation, standard_deviation, lifetime, seed=None):
+    if seed is None:
+        seed = 1
+    t = np.arange(len(excitation)) / len(excitation)
+    np.random.seed(seed)
+    noise = np.random.normal(0, standard_deviation, len(excitation))
+    exponential_fading_noise = noise * np.exp(-t / lifetime)
+    exponential_fading_noise[0] = 1
+    return exponential_fading_noise
+
+
+def diracs(excitation, dirac_positions_array):
+    h = np.zeros(len(excitation))
+    for i in dirac_positions_array:
+        h[i] = 1
+    return h
+
+# generate some impulse responses end
+
 # Help functions
+
+
 def chained(*systems):
     def inner(arg):
         for system in systems:
