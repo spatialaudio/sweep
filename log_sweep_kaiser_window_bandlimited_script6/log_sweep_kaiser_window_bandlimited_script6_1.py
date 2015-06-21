ee01c756255b1f127cf477e0cdaf85014bd0efc8
franzpl <franz.plocksties@uni-rostock.de>
2015-06-21 16:40:55 +0200

    First Add

---
+++ log_sweep_kaiser_window_bandlimited_script6/log_sweep_kaiser_window_bandlimited_script6_1.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+"""The influence of windowing of log. sweep signals when using a
+   Kaiser Window by fixing beta (=7) and fade_in (=0).
+   fstart = 100 Hz
+   fstop =  5000 Hz
+   Deconvolution: Unwindowed
+
+"""
+
+
+import sys
+sys.path.append('..')
+
+import measurement_chain
+import plotting
+import calculation
+import generation
+import matplotlib.pyplot as plt
+import windows
+from scipy.signal import lfilter
+import numpy as np
+
+
+# Parameters of the measuring system
+
+fs = 44100
+
+fstart = 100
+fstop = 5000
+duration = 1
+pad = 4
+
+# Generate excitation signal
+
+excitation = generation.log_sweep(fstart, fstop, duration, fs)
+N = len(excitation)
+
+# Noise in measurement chain
+
+noise_level_db = -30.
+noise = measurement_chain.additive_noise(noise_level_db)
+
+# FIR-Filter-System
+
+dirac_system = measurement_chain.convolution([1.0])
+
+# Combinate system elements
+
+system = measurement_chain.chained(dirac_system, noise)
+
+# Lists
+beta = 7
+fade_in = 0
+fade_out_list = np.arange(0, 1001, 1)
+
+# Spectrum of dirac for reference
+
+dirac = np.zeros(pad * fs)
+dirac[0] = 1
+dirac_f = np.fft.rfft(dirac)
+
+
+def get_results(fade_out):
+    excitation_windowed = excitation * windows.window_kaiser(N,
+                                                             fade_in,
+                                                             fade_out,
+                                                             fs, beta)
+    excitation_zeropadded = generation.zero_padding(excitation, pad, fs)
+    excitation_windowed_zeropadded = generation.zero_padding(
+        excitation_windowed, pad, fs)
+    system_response = system(excitation_windowed_zeropadded)
+    ir = calculation.deconv_process(excitation_zeropadded,
+                                    system_response,
+                                    fs)
+    return ir
+
+
+with open("log_sweep_kaiser_window_bandlimited_script6_1.txt", "w") as f:
+    for fade_out in fade_out_list:
+        ir = get_results(fade_out)
+        pnr = calculation.pnr_db(ir[0], ir[1:4 * fs])
+        spectrum_distance = calculation.vector_distance(
+            dirac_f, np.fft.rfft(ir[:pad * fs]))
+        f.write(
+            str(fade_out) + " " + str(pnr) +
+            " " + str(spectrum_distance) + " \n")
---
+++ log_sweep_kaiser_window_bandlimited_script6/merge_scripts.py
@@ -0,0 +1,61 @@
+""" Script to merge scripts"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+script6 = np.genfromtxt('log_sweep_kaiser_window_bandlimited_script6.txt')
+
+script6_1 = np.genfromtxt('log_sweep_kaiser_window_bandlimited_script6_1.txt')
+
+fade_out_list = script6[:, 0]
+
+# Script6
+
+pnr_list = script6[:, 1]
+spectrum_distance_list = script6[:, 2]
+
+# Script6_1 (unwindowed deconvolution)
+
+pnr_unwindowed_deconvolution_list = script6_1[:, 1]
+spectrum_distance_unwindowed_deconvolution_list = script6_1[:, 2]
+
+
+plt.plot(fade_out_list, pnr_list, label='Deconvolution: Excitation windowed')
+
+plt.plot(
+    fade_out_list,
+     pnr_unwindowed_deconvolution_list,
+     label='Deconvolution: Excitation unwindowed')
+plt.grid()
+plt.title('Peak to noise ratio depending on Fade out')
+plt.xlabel('Fade out / ms')
+plt.ylabel('Peak to noise ratio / dB')
+plt.ticklabel_format(useOffset=False)
+plt.legend(loc='upper right')
+plt.xlim([-10, 1000])
+plt.savefig('pnr.png')
+plt.close()
+
+
+NFFT_dirac = 88201
+max_measurement = 7040966.76862
+
+plt.plot(
+    fade_out_list,
+     -10 * np.log10(1 / NFFT_dirac * np.abs(
+         np.asarray(spectrum_distance_list) / max_measurement)),
+     label='Deconvolution: Excitation windowed')
+
+plt.plot(fade_out_list,
+         -10 * np.log10(1 / NFFT_dirac * np.abs(np.asarray(spectrum_distance_unwindowed_deconvolution_list) /
+                                                max_measurement)), label='Deconvolution: Excitation unwindowed')
+
+
+plt.grid()
+plt.title('Spectrum Distance depending on Fade in')
+plt.xlabel('Fade out / ms')
+plt.ylabel('(Spectrum Distance / max(Spectrum Distance)) / dB')
+plt.ticklabel_format(useOffset=False)
+plt.legend(loc='center right')
+plt.xlim([-10, 1000])
+plt.savefig('spectral_distance.png')
