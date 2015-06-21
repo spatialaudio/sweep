e186d49f5546f9e965b1523d8ef7d9d43fe53399
franzpl <franz.plocksties@uni-rostock.de>
2015-06-21 16:40:04 +0200

    First Add

---
+++ log_sweep_kaiser_window_bandlimited_script5/merge_scripts.py
@@ -0,0 +1,61 @@
+""" Script to merge scripts"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+script5 = np.genfromtxt('log_sweep_kaiser_window_bandlimited_script5.txt')
+
+script5_1 = np.genfromtxt('log_sweep_kaiser_window_bandlimited_script5_1.txt')
+
+fade_in_list = script5[:, 0]
+
+# Script5
+
+pnr_list = script5[:, 1]
+spectrum_distance_list = script5[:, 2]
+
+# Script5_1 (unwindowed deconvolution)
+
+pnr_unwindowed_deconvolution_list = script5_1[:, 1]
+spectrum_distance_unwindowed_deconvolution_list = script5_1[:, 2]
+
+
+plt.plot(fade_in_list, pnr_list, label='Deconvolution: Excitation windowed')
+
+plt.plot(
+    fade_in_list,
+    pnr_unwindowed_deconvolution_list,
+    label='Deconvolution: Excitation unwindowed')
+plt.grid()
+plt.title('Peak to noise ratio depending on Fade in')
+plt.xlabel('Fade in / ms')
+plt.ylabel('Peak to noise ratio / dB')
+plt.ticklabel_format(useOffset=False)
+plt.legend(loc='lower left')
+plt.xlim([-10, 1000])
+plt.savefig('pnr.png')
+plt.close()
+
+
+NFFT_dirac = 88201
+max_measurement = 7040966.76862
+
+plt.plot(
+    fade_in_list,
+     -10 * np.log10(1 / NFFT_dirac * np.abs(
+         np.asarray(spectrum_distance_list) / max_measurement)),
+     label='Deconvolution: Excitation windowed')
+
+plt.plot(fade_in_list,
+         -10 * np.log10(1 / NFFT_dirac * np.abs(np.asarray(spectrum_distance_unwindowed_deconvolution_list) /
+                                                max_measurement)), label='Deconvolution: Excitation unwindowed')
+
+
+plt.grid()
+plt.title('Spectrum Distance depending on Fade in')
+plt.xlabel('Fade in / ms')
+plt.ylabel('(Spectrum Distance / max(Spectrum Distance)) / dB')
+plt.ticklabel_format(useOffset=False)
+plt.legend(loc='center right')
+plt.xlim([-10, 1000])
+plt.savefig('spectrale_distance.png')
