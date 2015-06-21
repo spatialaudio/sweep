887852b7a54c7cf3e13e8fd47b11aba9a22b9212
franzpl <franz.plocksties@uni-rostock.de>
2015-06-21 16:58:27 +0200

    Add some functions

--- measurement_chain.py
+++ measurement_chain.py
@@ -8,9 +8,19 @@ def bandpass(lower_bound, higher_bound, fs, order):
     def inner(data):
         b, a = _butter_bandpass(lower_bound, higher_bound, fs, order)
         return lfilter(b, a, data)
-    inner.name = "{}.-order bandpass with lower_bound = {} Hz and higher_bound = {} Hz".format(
-        order, lower_bound, higher_bound)
+    inner.name = "{}.-order bandpass with f_L = {} Hz, f_H = {} Hz".format(
+                 order, lower_bound, higher_bound)
     inner.filename = "bp{}_{}_{}".format(order, lower_bound, higher_bound)
+    return inner
+
+
+def bandstop(lower_bound, higher_bound, fs, order):
+    def inner(data):
+        b, a = _butter_bandstop(lower_bound, higher_bound, fs, order)
+        return lfilter(b, a, data)
+    inner.name = "{}.-order bandstop with f_L = {} Hz, f_H = {} Hz".format(
+                 order, lower_bound, higher_bound)
+    inner.filename = "bs{}_{}_{}".format(order, lower_bound, higher_bound)
     return inner
 
 
@@ -23,13 +33,23 @@ def additive_noise(noise_level_db, seed=1):
     return inner
 
 
-def anti_aliasing_filter(cutoff, fs, order):
+def lowpass(cutoff, fs, order):
     def inner(data):
         b, a = _butter_lowpass(cutoff, fs, order=order)
         return lfilter(b, a, data)
-    inner.name = "anti-aliasing filter with fc = {} and {}.order".format(
+    inner.name = "lowpass-filter with fc = {} and {}.order".format(
         cutoff, order)
-    inner.filename = "aaf_fc{}_order{} ".format(cutoff, order)
+    inner.filename = "lpf_fc{}_order{} ".format(cutoff, order)
+    return inner
+
+
+def highpass(cutoff, fs, order):
+    def inner(data):
+        b, a = _butter_highpass(cutoff, fs, order=order)
+        return lfilter(b, a, data)
+    inner.name = "high-filter with fc = {} and {}.order".format(
+        cutoff, order)
+    inner.filename = "hpf_fc{}_order{} ".format(cutoff, order)
     return inner
 
 
@@ -52,44 +72,12 @@ def moving_average(N):
     return inner
 
 
-def convolution(ir):  # TODO: set string
+def convolution(ir):
     def inner(data):
         return fftconvolve(data, ir)[:len(data)]
-    inner.name = "{}".format("IR")  # inner.name = string
+    inner.name = "{}".format("IR")
     inner.filename = "{}".format("IR")
     return inner
-
-
-def lfilter_new(b, a): # TODO: add to fir-filter
-    def inner(data):
-        return lfilter(b, a, data)[:len(data)]
-    inner.name = "{}".format("IR")  # inner.name = string
-    inner.filename = "{}".format("IR")
-    return inner
-
-
-def exponential_decay(duration_seconds,
-                      lifetime_seconds,
-                      noise_level_db,
-                      fs,
-                      seed=1):
-    t = np.arange(0, duration_seconds, 1 / fs)
-    noise = calculation.noise_db(noise_level_db, duration_seconds * fs, seed)
-    exponential_fading_noise = noise * np.exp(-t / lifetime_seconds)
-    exponential_fading_noise[0] = 1
-    return exponential_fading_noise
-
-
-def diracs(array):
-    a = np.asarray(array)
-    y = np.zeros(a.max() + 1)
-    y[a] = 1
-    return y
-
-
-def bandstop(lower_bound, higher_bound, fs, order):
-    b, a = _butter_bandstop(lower_bound, higher_bound, fs, order)
-    return b, a
 
 
 # Help functions
@@ -107,6 +95,12 @@ def chained(*systems):
 def _butter_lowpass(cutoff, fs, order):
     wc = cutoff / (fs / 2)
     b, a = butter(order, wc, btype='lowpass')
+    return b, a
+
+
+def _butter_highpass(cutoff, fs, order):
+    wc = cutoff / (fs / 2)
+    b, a = butter(order, wc, btype='highpass')
     return b, a
 
 
